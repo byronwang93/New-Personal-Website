399eed85c117b0e79e26e85d37dfd8492f9f9bf3
byronwang93 <byronwang123919@gmail.com>
2021-12-31 00:23:37 -0800

    added click sound every time you click the dark mode or light mode button

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ var icon = document.getElementById("toggle");
 
 icon.onclick = function() {
     document.body.classList.toggle("dark-theme");
+
+    var audio = new Audio();
+    audio.src = "click.mp3";
+    audio.play();
+
     if (document.body.classList.contains("dark-theme")) {
         icon.innerHTML = "Too Dark? <i class='far fa-sun'></i>";
     } else {
         icon.innerHTML = "Too Bright? <i class='far fa-moon'></i>";
     }
-}
+} 
