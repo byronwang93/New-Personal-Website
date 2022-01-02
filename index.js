13e861a3b8b031dc03de6e1b9c4578ec6ea88ada
byronwang93 <byronwang123919@gmail.com>
2022-01-02 13:52:55 -0800

    test

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 // var icons = document.getElementsByClassName("toggle");
 // var icon = icons[0];
 
-var icon = document.getElementsByClassName("mainToggle");
-
-icon.onclick = function() {
-    document.body.classList.toggle("dark-theme");
-
-    var audio = new Audio();
-    audio.src = "click.mp3";
-    audio.play();
-
-    // if (document.body.classList.contains("dark-theme")) {
-    //     icon.innerHTML = "Too Dark? <i class='far fa-sun'></i>";
-    // } else {
-    //     icon.innerHTML = "Too Bright? <i class='far fa-moon'></i>";
-    // }
-} 
-
 // var elements = document.getElementsByTagName("form");
 var elements = document.getElementsByClassName("toggle");
 
@@ -48,3 +32,20 @@ function toggleFunction() {
         } 
     }
 }
+
+
+var icon = document.getElementsByClassName("mainToggle");
+
+icon[0].onclick = function() {
+    document.body.classList.toggle("dark-theme");
+
+    var audio = new Audio();
+    audio.src = "click.mp3";
+    audio.play();
+
+    // if (document.body.classList.contains("dark-theme")) {
+    //     icon.innerHTML = "Too Dark? <i class='far fa-sun'></i>";
+    // } else {
+    //     icon.innerHTML = "Too Bright? <i class='far fa-moon'></i>";
+    // }
+} 
